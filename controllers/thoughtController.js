1f524cfd614f26e848e6a8363549b65ac6eb2231
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-19 17:49:13 -0500

    implemented GET and POST thought routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,28 @@
+const { User, Thought, Reaction } = require('../models');
+
+// GET all thoughts
+const getThoughts = async (req, res) => {
+    try {
+        const thoughtData = await Thought.find();
+        res.json(thoughtData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+// POST a new thought
+const createThought = async (req, res) => {
+    try {
+        const thoughtData = await Thought.create(req.body);
+        const userData = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thoughtData._id } },
+            { new: true }
+        )
+        res.json(thoughtData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+module.exports = { getThoughts, createThought };
