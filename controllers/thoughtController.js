9e286dd37fcb321dd9573bba3f589670c18461c2
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-19 22:54:48 -0500

    can update and delete thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -40,12 +40,30 @@ const createThought = async (req, res) => {
 
 // PUT to update a thought by its _id
 const updateThought = async (req, res) => {
-
+    try {
+        const thoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            req.body,
+            { new: true }
+        );
+        !thoughtData 
+            ? res.status(404).json({ message: 'No thought found with that ID' }) 
+            : res.json(thoughtData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // DELETE a thought by its _id
 const deleteThought = async (req, res) => {
-
+    try {
+        const thoughtData = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
+        !thoughtData 
+            ? res.status(404).json({ message: 'No thought found with that ID' }) 
+            : res.json(thoughtData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // POST to add a reaction to a thought
